dc5d9db0629bf6eb03f7329429933956e96ee407
Taden Marston <113535943+StrikorDawn@users.noreply.github.com>
2025-01-11 17:14:09 -0700

    ReUpload data structure 1

---
+++ ds1-problem/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //TODO use what you have learned to make a Bug submission feature for our music app.
+        Queue<string> bugQueue = new Queue<string>();
+        //This is an example of how you could format your test cases.
+        bugQueue.Enqueue("BugID:0001 - Cannot adjust volume of songs");
+        //TODO create at least 3 additional items to be added to the bugQueue for testing.
+        
+        
+        
+        
+        //----------------------------------------------------
+        Console.WriteLine("Current Bug Queue:");
+        foreach (var bug in bugQueue)
+        {
+            Console.WriteLine(bug);
+        }
+        //The expected result should be the test cases in the exact same order you put them in.
+        //----------------------------------------------------
+        //Now we are going to practice Dequeueing from the bugQueue
+        //TODO Write at least 2 Dequeue statements
+        bugQueue.Dequeue();
+        
+        //----------------------------------------------------
+        Console.WriteLine("Updated Bug Queue:");
+        foreach (var bug in bugQueue)
+        {
+            Console.WriteLine(bug);
+        }
+        //The expected result should be the test cases minus at least the first 2 in the exact same order you put them in.
+        //----------------------------------------------------
+    }
+}
---
+++ ds1-solution/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a queue to represent the bug submission queue
+        Queue<string> bugQueue = new Queue<string>();
+
+        // Simulate bug submissions by adding them to the queue
+        bugQueue.Enqueue("Bug 1: UI glitch on login screen");
+        bugQueue.Enqueue("Bug 2: Error message not displaying properly");
+        bugQueue.Enqueue("Bug 3: Application crashes when clicking button");
+
+        // Display the current bug queue
+        Console.WriteLine("Current Bug Queue:");
+
+        foreach (var bug in bugQueue)
+        {
+            Console.WriteLine(bug);
+        }
+
+        bugQueue.Dequeue();
+        
+
+        // Simulate fixing bugs by dequeuing them (handling them one by one)
+        Console.WriteLine("Updated Bug Queue:");
+        foreach (var bug in bugQueue)
+        {
+            Console.WriteLine(bug);
+        }
+        
+    }
+}
